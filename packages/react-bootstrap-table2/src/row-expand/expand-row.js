6019ab8ce876398d2923fe18439a60f538e8b84f
Joseph Han <hanbingbing1@gmail.com>
2021-01-12 15:58:07 +1300

    Fix Bug

--- packages/react-bootstrap-table2/src/row-expand/expand-row.js
+++ packages/react-bootstrap-table2/src/row-expand/expand-row.js
@@ -3,25 +3,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
 
-const ExpandRow = ({ children, expanded, onClosed, className, ...rest }) => (
-  <tr>
-    <td className={ cs('reset-expansion-style', className) } { ...rest }>
-      <CSSTransition
-        appear
-        in={ expanded }
-        timeout={ 400 }
-        classNames="row-expand-slide"
-        onExited={ onClosed }
-      >
-        <div>
-          <div className="row-expansion-style">
-            { children }
+const ExpandRow = ({ children, expanded, onClosed, className, ...rest }) => {
+  const nodeRef = React.useRef(null);
+  return (
+    <tr>
+      <td className={ cs('reset-expansion-style', className) } { ...rest }>
+        <CSSTransition
+          nodeRef={ nodeRef }
+          appear
+          in={ expanded }
+          timeout={ 400 }
+          classNames="row-expand-slide"
+          onExited={ onClosed }
+        >
+          <div>
+            <div className="row-expansion-style">
+              { children }
+            </div>
           </div>
-        </div>
-      </CSSTransition>
-    </td>
-  </tr>
-);
+        </CSSTransition>
+      </td>
+    </tr>
+  );
+};
 
 ExpandRow.propTypes = {
   children: PropTypes.node,
